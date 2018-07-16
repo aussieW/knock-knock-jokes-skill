de34b5683a6d8b2f121730eddd922906a85bb5c1
aussieW <austin.j.wilson@gmail.com>
2018-07-17 06:03:25 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,21 @@
+from os.path import abspath, join, dirname
+from random import choice
+
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util import wait_while_speaking
+
+from mycroft.util.log import getLogger
+LOGGER = getLogger(__name__)
 
 
 class KnockKnockJokes(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.jokes = []
+        self.joke = None
+        self.stage = 0
+        self.path_to_joke_file = join(abspath(dirname(__file__)), 'jokes', 'jokes.txt')
 
     @intent_file_handler('jokes.knock.knock.intent')
     def handle_jokes_knock_knock(self, message):
